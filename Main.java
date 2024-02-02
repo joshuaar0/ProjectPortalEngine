a2327d765b264fdb249281300a9de1f2cfd23ab2
Joshuaar0 <jargomaniz02@aurora.edu>
2024-02-02 10:14:37 -0600

    Added for loop to ten!

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
-public class Main {
-    public static void main(String[] args) {
+public class Main
+{
+    public static void main(String[] args)
+    {
         System.out.println("Hello world!");
+        int ten = 10;
+        for (int i = 0; i < ten; i++)
+            System.out.println(i);
     }
 }
